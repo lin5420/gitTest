8f77864f4c67b85526e973207b2c2ca014b90e88
lin5420 <30519505+lin5420@users.noreply.github.com>
2017-07-28 15:33:17 +0800

    test commmitvbvbvb

--- src/main/java/com/lynn/test/Test.java
+++ src/main/java/com/lynn/test/Test.java
@@ -3,7 +3,7 @@ package com.lynn.test;
 public class Test {
     public static void main(String [] args){
         for (int i = 0; i <10 ; i++) {
-            System.out.println(i);
+            System.out.println(i +"xxxxxxxxxxxxxx");
         }
     }
 }
